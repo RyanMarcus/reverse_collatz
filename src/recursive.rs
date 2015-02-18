fab95df15e7e81479896211c42cf8e09b471d789
Ryan Marcus <ryan@rmarcus.info>
2015-02-17 20:48:31 -0500

    Added license

--- src/recursive.rs
+++ src/recursive.rs
@@ -1,5 +1,26 @@
 #[allow(dead_code)]
 
+/*
+
+Copyright 2015 Ryan Marcus
+
+This file is part of reverse_collatz.
+
+reverse_collatz is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+reverse_collatz is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with reverse_collatz.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 pub fn calculate(steps: i64) -> i64 {
     return rcalculate(1, steps);
 }
--- src/triangular_expansion.rs
+++ src/triangular_expansion.rs
@@ -1,3 +1,23 @@
+/*
+
+Copyright 2015 Ryan Marcus
+
+This file is part of reverse_collatz.
+
+reverse_collatz is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+reverse_collatz is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with reverse_collatz.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #[allow(dead_code)]
 
 pub fn calculate(steps: i64) -> i64 {
